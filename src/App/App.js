26ffeb33bbb04511fcfcd7de68928ac52655d099
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-08-29 18:05:54 -0500

    initial setup

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  render() {
+
+    return (
+      <div className='root'>
+
+
+        <p>hello</p>
+
+      </div> // end root
+    ); // end return
+  } // end render 
+} // end class
+
+export default App;
