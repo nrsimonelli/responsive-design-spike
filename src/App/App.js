da534a5e989ffb5d81b0879a4d2c09c578de4a75
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-08-29 23:40:45 -0500

    images and responsive design

--- src/App/App.js
+++ src/App/App.js
@@ -6,10 +6,80 @@ class App extends Component {
   render() {
 
     return (
-      <div className='root'>
-
-
-        <p>hello</p>
+      <div className='root b'>
+        <div id='HEADER' className='nav'>
+          <h1>dani b's pops </h1>
+        </div>
+        <div id='BODY' className='material'>
+          <div className='container'>
+            <div className='card pop-1'>
+              
+            </div>
+            <div className='card pop-2'>
+              
+            </div>
+            <div className='card pop-4'>
+              
+            </div>
+            <div className='card pop-5'>
+              
+            </div>
+            <div className='card pop-6'>
+              
+            </div>
+            <div className='card pop-22'>
+              
+            </div>
+            
+            <div className='card pop-8'>
+              
+            </div>
+            <div className='card pop-9'>
+              
+            </div>
+            <div className='card pop-10'>
+              
+            </div>
+            <div className='card pop-11'>
+              
+            </div>
+            <div className='card pop-12'>
+              
+            </div>
+            <div className='card pop-13'>
+              
+            </div>
+            <div className='card pop-14'>
+              
+            </div>
+            <div className='card pop-15'>
+              
+            </div>
+            <div className='card pop-16'>
+              
+            </div>
+            <div className='card pop-17'>
+              
+            </div>
+            <div className='card pop-18'>
+              
+            </div>
+            <div className='card pop-19'>
+              
+            </div>
+            <div className='card pop-20'>
+              
+            </div>
+            <div className='card pop-21'>
+              
+            </div>
+            <div className='card pop-7'>
+              
+            </div>
+            
+          </div>
+        </div>
+        <p>post body</p>
 
       </div> // end root
     ); // end return
